892ac967e667b2ba3b1c88353a38a900e756e08a
shadowchj <452332341@qq.com>
2018-05-09 15:28:14 +0800

    update readme

--- www/config_default.py
+++ www/config_default.py
@@ -7,8 +7,8 @@ configs = {
 		'host':'127.0.0.1',
 		'port':3306,
 		'user':'root',
-		'password':'xxx',
-		'db':'xxx',
+		'password':'fakshadowCHJ666',
+		'db':'awsome',
 	},
 	'session':{
 		'secret':'asdfg'
